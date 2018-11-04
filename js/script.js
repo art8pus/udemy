941794bcd1ad2ade297cadcc44fa5907b59e813a
art8pus <seedeaver@gmail.com>
2018-11-04 18:48:28 +0100

    adding starting project

---
+++ js/script.js
@@ -0,0 +1,48 @@
+//  Memory Heap
+/*
+const a = 1;
+const b = 10;
+const c = 100;
+*/
+
+//  Call Stack
+/*
+console.log("1");
+console.log("2");
+console.log("3");
+*/
+/*
+const one = () =>{
+    const two = () =>{
+        console.log("4");
+    }
+    two();
+}
+*/
+
+//  Recursion
+
+/*
+function for(){
+    foo()
+}
+foo()
+*/
+
+//  Call Stack and asynchronous programming
+/*
+console.log("1");
+setTimeout(() =>{
+    console.log("2");
+}, 2000);
+console.log("3");
+*/
+
+//  CALL STACK
+
+//  WEB API
+
+//  CALLBACK QUEUE
+
+//  EVENT LOOP
+
